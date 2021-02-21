7e95e634451160581e7bfa5136b3cda884132376
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2021-02-21 18:54:47 +0900

    編集画面と削除の過程に移る

--- app/src/main/java/app/ito/akki/checkwork/DetailActivity.kt
+++ app/src/main/java/app/ito/akki/checkwork/DetailActivity.kt
@@ -2,8 +2,15 @@ package app.ito.akki.checkwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.Realm
 
 class DetailActivity : AppCompatActivity() {
+
+
+    //Realmのデータの取得はlazyで行う
+    private val realm: Realm by lazy {
+        Realm.getDefaultInstance()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
